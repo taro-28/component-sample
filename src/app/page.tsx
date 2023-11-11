6cfd23c59cb1f2d2d0af5b8a416b53579ac82c05
taro-28 <tarotaro0208@gmail.com>
2023-11-11 16:50:54 +0900

    Add faker.js dependency and generate fake user data for DataTable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,77 +2,38 @@
 
 import * as React from "react";
 import { DataTable } from "@/components/DataTable";
+import { faker } from "@faker-js/faker";
 
-type Payment = {
+type User = {
   id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
+  name: string;
   email: string;
-  stringArray: string[];
-  object: {
-    id: string;
-    name: string;
-  };
+  age: number;
+  avatar: string;
+  birthdate: Date;
 };
 
-const data: Payment[] = [
-  {
-    id: "m5gr84i9",
-    amount: 316,
-    status: "success",
-    email: "ken99@yahoo.com",
-    stringArray: ["todo1", "todo2"],
-    object: {
-      id: "m5gr84i9",
-      name: "ken99",
-    },
-  },
-  {
-    id: "3u1reuv4",
-    amount: 242,
-    status: "success",
-    email: "Abe45@gmail.com",
-    stringArray: ["todo1", "todo2"],
-    object: {
-      id: "3u1reuv4",
-      name: "Abe45",
-    },
-  },
-  {
-    id: "derv1ws0",
-    amount: 837,
-    status: "processing",
-    email: "Monserrat44@gmail.com",
-    stringArray: ["todo1", "todo2"],
-    object: {
-      id: "derv1ws0",
-      name: "Monserrat44",
-    },
-  },
-  {
-    id: "5kma53ae",
-    amount: 874,
-    status: "success",
-    email: "Silas22@gmail.com",
-    stringArray: ["todo1", "todo2"],
-    object: {
-      id: "5kma53ae",
-      name: "Silas22",
-    },
-  },
-  {
-    id: "bhqecj4p",
-    amount: 721,
-    status: "failed",
-    email: "carmella@hotmail.com",
-    stringArray: ["todo1", "todo2"],
-    object: {
-      id: "bhqecj4p",
-      name: "carmella",
-    },
-  },
-];
+const createUser = (): User => ({
+  id: faker.string.uuid(),
+  name: faker.internet.userName(),
+  email: faker.internet.email(),
+  age: faker.number.int({ min: 18, max: 100 }),
+  avatar: faker.image.avatar(),
+  birthdate: faker.date.birthdate(),
+});
+
+const data: User[] = faker.helpers.multiple(createUser, {
+  count: 100,
+});
 
 export default function DataTableDemo() {
-  return <DataTable data={data} />;
+  return (
+    <div className="mx-4 space-y-2">
+      <h1 className="text-xl">Component Samples</h1>
+      <div>
+        <h2 className="text-lg">Data Table</h2>
+        <DataTable data={data} />
+      </div>
+    </div>
+  );
 }
